f398e8c10b3c6cd2acff988421799021f2070bc8
Muhammad Hassan <hassanaqve@gmail.com>
2022-07-27 11:10:25 +0500

    .

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+
+  const [btc, setBtc] = useState()
+  const [Quan, setQuan] = useState()
+  const [price, setPrice] = useState()
+  const [result, setResult] = useState()
+
+  const onTextchange = (e) => {
+    console.log('log', e.target.value)
+    if (e.target.value) {
+      setQuan(e.target.value)
+      axios
+        .get(
+          'https://api.alternative.me/v2/ticker/?convert=USD'
+        )
+        .then(res => {
+          // setBtc(res.data.bitcoin.usd);
+          console.log(res.data['data'][1]['quotes']['USD']['price']);
+          setPrice(res.data['data'][1]['quotes']['USD']['price']);
+
+
+        })
+        .catch(error => console.log(error));
+
+      console.log('result', Quan * price)
+
+      setResult(Quan * price)
+    }
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <h1>Simple Bitcoin / USD Converter</h1>
+      <div >
+        <input type="text" id="fname" onChange={onTextchange} ></input>
+        <label> BTC </label>
+        <br /> <br />
+        <label id="usd" >{result}</label>
+        <label > USD </label>
+
+      </div>
+    </div >
   );
 }
 
